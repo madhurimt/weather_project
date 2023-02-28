ef92a8a7f14c33020fa1814e619adacaa867aa2a
madhurimt <madhuri.maniktahla@gmail.com>
2023-02-28 22:50:22 +1100

    adding env variables

--- main.js
+++ main.js
@@ -6,12 +6,76 @@ const api_key = process.env.API_KEY;
 const user_input = document.querySelector(".search");
 const msg = document.querySelector(".error_msg");
 
-user_input.addEventListener("keypress", getResults);
+if (user_input !== null) {
+  user_input.addEventListener("keypress", getResults);
+}
+
+let geo_code = {
+  reverseGeocode: function (latitude, longitude) {
+    let api_key_geo = process.env.API_KEY_GEO;
+    let api_url = "https://api.opencagedata.com/geocode/v1/json";
+
+    let request_url =
+      api_url +
+      "?" +
+      "key=" +
+      api_key_geo +
+      "&q=" +
+      encodeURIComponent(latitude + "," + longitude) +
+      "&pretty=1" +
+      "&no_annotations=1";
+
+    // see full list of required and optional parameters:
+    // https://opencagedata.com/api#forward
+
+    let request = new XMLHttpRequest();
+    request.open("GET", request_url, true);
+
+    request.onload = function () {
+      // see full list of possible response codes:
+      // https://opencagedata.com/api#codes
+
+      if (request.status === 200) {
+        // Success!
+        console.log("i m in here");
+        var data = JSON.parse(request.responseText);
+        console.log(data.results[0].components.city); // print the location
+        lonlatResults(data.results[0].components.city);
+      } else if (request.status <= 500) {
+        // We reached our target server, but it returned an error
+
+        console.log("unable to geocode! Response code: " + request.status);
+        var data = JSON.parse(request.responseText);
+        console.log("error msg: " + data.status.message);
+      } else {
+        console.log("server error");
+      }
+    };
+
+    request.onerror = function () {
+      // There was a connection error of some sort
+      console.log("unable to connect to server");
+    };
+
+    request.send(); // make the request
+  },
+  getLocation: function () {
+    function success(data) {
+      console.log(data);
+      geo_code.reverseGeocode(data.coords.latitude, data.coords.longitude);
+    }
+    if (navigator.geolocation) {
+      console.log("i m in here");
+      navigator.geolocation.getCurrentPosition(success, console.error);
+    } else {
+      fetch(`${APP_URL}q=melbourne&units=metric&appid=${api_key}`)
+        .then(convertToJSObject)
+        .then(getData);
+    }
+  },
+};
 
 //getting melbourne results directly on the landing page
-fetch(`${APP_URL}q=melbourne&units=metric&appid=${api_key}`)
-  .then(convertToJSObject)
-  .then(getData);
 
 function getResults(e) {
   if (e.keyCode == 13) {
@@ -46,3 +110,5 @@ function getData(data) {
   let range = document.querySelector(".temp_range");
   range.textContent = `${data.main.temp_min}°C/ ${data.main.temp_max}°C`;
 }
+
+geo_code.getLocation();
